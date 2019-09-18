a5e620de4e2d48fc46f0f0d95c6d7ec0d4c413f4
Aaron Davis <adbe455@gmail.com>
2019-09-18 12:22:28 -0400

    Updates

---
+++ precache-manifest.5c08b0e2d3599f5986b1fede72a26365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0921662103e128bacb86dfc45704b3cd",
+    "url": "/pernClient/index.html"
+  },
+  {
+    "revision": "df0a3bee4ca64acf071c",
+    "url": "/pernClient/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "40c388eb7fee902174f7",
+    "url": "/pernClient/static/css/main.4d650b70.chunk.css"
+  },
+  {
+    "revision": "df0a3bee4ca64acf071c",
+    "url": "/pernClient/static/js/2.e1406b8f.chunk.js"
+  },
+  {
+    "revision": "40c388eb7fee902174f7",
+    "url": "/pernClient/static/js/main.7926c5a3.chunk.js"
+  },
+  {
+    "revision": "6f4b6a581ebb2ecee09f",
+    "url": "/pernClient/static/js/runtime~main.c1d528c7.js"
+  }
+]);
